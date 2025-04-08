340b905831618b73be2d8fac91757273b1a40153
Aleš Kalfas <kalfas.ales@gmail.com>
2025-04-08 11:48:23 +0200

    fix: copyright nwa
    Signed-off-by: Aleš Kalfas <kalfas.ales@gmail.com>

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,5 @@
 /**
- * Copyright © BeeAI a Series of LF Projects, LLC
+ * Copyright 2025 © BeeAI a Series of LF Projects, LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
