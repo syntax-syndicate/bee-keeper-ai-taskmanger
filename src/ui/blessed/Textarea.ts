dfc928149e25228386444beaec4e0cefd2cd4c04
Petr Bulánek <bulanek.petr@gmail.com>
2025-05-08 09:10:24 +0200

    feat: control textarea with keyboard
    Signed-off-by: Petr Bulánek <bulanek.petr@gmail.com>

--- src/ui/blessed/Textarea.ts
+++ src/ui/blessed/Textarea.ts
@@ -1,5 +1,4 @@
 import blessed from "neo-blessed";
-import unicode from "neo-blessed/lib/unicode.js";
 
 const nextTick = global.setImmediate || process.nextTick.bind(process);
 
@@ -13,7 +12,13 @@ const InputElement = blessed.input as unknown as new (
   opts: TextareaOptions,
 ) => TextareaElement & {
   _value: string;
-  _clines: [];
+  _clines: {
+    real: string[];
+    fake: string[];
+    rtof: number[];
+    length: number;
+    [index: number]: string;
+  };
   _reading: boolean;
   _getCoords: () => BlessedElement["lpos"];
   _render: () => Coords;
@@ -28,6 +33,8 @@ const InputElement = blessed.input as unknown as new (
 export class Textarea extends InputElement {
   private __listener?: (ch: any, key: IKeyEventArg) => void;
   private __done?: (err: any, value?: string | null) => void;
+  private offsetY: number;
+  private offsetX: number;
 
   constructor(opts: TextareaOptions) {
     const options: TextareaOptions = {
@@ -41,6 +48,9 @@ export class Textarea extends InputElement {
 
     this.options = options;
     this.value = options.value || "";
+
+    this.offsetY = 0;
+    this.offsetX = 0;
 
     this.on("resize", this._updateCursor.bind(this));
     this.on("move", this._updateCursor.bind(this));
@@ -82,25 +92,27 @@ export class Textarea extends InputElement {
     }
 
     const lpos = get ? this.lpos : this._getCoords();
+
     if (!lpos) {
       return;
     }
 
-    let last = this._clines[this._clines.length - 1],
-      line;
+    const currentLine = this._clines.length - 1 + this.offsetY;
     const program = this.screen.program;
+    let currentText = this._clines[currentLine];
+    let line;
 
     // Stop a situation where the textarea begins scrolling
     // and the last cline appears to always be empty from the
     // _typeScroll `+ '\n'` thing.
     // Maybe not necessary anymore?
-    if (last === "" && this.value[this.value.length - 1] !== "\n") {
-      last = this._clines[this._clines.length - 2] || "";
+    if (currentText === "" && this.value[this.value.length - 1] !== "\n") {
+      currentText = this._clines[currentLine - 1] || "";
     }
 
     line = Math.min(
-      this._clines.length - 1 - (this.childBase || 0),
-      lpos.yl - lpos.yi - (this.iheight as number) - 1,
+      currentLine - (this.childBase || 0),
+      lpos.yl - lpos.yi - Number(this.iheight) - 1,
     );
 
     // When calling clearValue() on a full textarea with a border, the first
@@ -108,11 +120,12 @@ export class Textarea extends InputElement {
     // positive.
     line = Math.max(0, line);
 
-    const cy = lpos.yi + (this.itop as number) + line;
+    const cy = lpos.yi + Number(this.itop) + line;
     const cx =
+      this.offsetX +
       lpos.xi +
-      (this.ileft as number) +
-      (this.strWidth(last) as unknown as number); // strWidth actually returns number
+      Number(this.ileft) +
+      Number(this.strWidth(currentText));
 
     // XXX Not sure, but this may still sometimes
     // cause problems when leaving editor.
@@ -139,10 +152,11 @@ export class Textarea extends InputElement {
 
   _typeScroll() {
     // XXX Workaround
-    const height = (this.height as number) - (this.iheight as number);
-    if (this._clines.length - this.childBase > height) {
-      this.scroll(this._clines.length);
-    }
+    // const height = Number(this.height) - Number(this.iheight);
+    // if (this._clines.length - this.childBase > height) {
+    const currentLine = this._clines.length - 1 + this.offsetY;
+    this.setScroll(currentLine);
+    // }
   }
 
   _listener(ch: any, key: Partial<IKeyEventArg>) {
@@ -152,18 +166,44 @@ export class Textarea extends InputElement {
     if (key.name === "return") {
       return;
     }
+
     if (key.name === "enter") {
       ch = "\n";
     }
+
+    const cursor = this.getCursor();
 
     // TODO: Handle directional keys.
     if (
       key.name === "left" ||
       key.name === "right" ||
       key.name === "up" ||
-      key.name === "down"
+      key.name === "down" ||
+      key.name === "end" ||
+      key.name === "home"
     ) {
-      // console.log(key.name);
+      if (key.name === "left") {
+        cursor.x--;
+      } else if (key.name === "right") {
+        cursor.x++;
+      }
+      if (key.name === "up") {
+        cursor.y--;
+      } else if (key.name === "down") {
+        cursor.y++;
+      }
+
+      if (key.name === "end") {
+        cursor.x = 0;
+      } else if (key.name === "home") {
+        const currentLine = this._clines.length - 1 + this.offsetY;
+        const currentLineLength = Number(
+          this.strWidth(this._clines[currentLine] ?? ""),
+        );
+        cursor.x = -currentLineLength;
+      }
+
+      this.moveCursor(cursor.x, cursor.y);
     }
 
     if (this.options.keys && key.ctrl && key.name === "e") {
@@ -177,19 +217,187 @@ export class Textarea extends InputElement {
     } else if (key.name === "backspace") {
       if (this.value.length) {
         if (this.screen.fullUnicode) {
-          if (unicode.isSurrogate(this.value, this.value.length - 2)) {
-            this.value = this.value.slice(0, -2);
-          } else {
-            this.value = this.value.slice(0, -1);
-          }
+          //
         } else {
-          this.value = this.value.slice(0, -1);
+          if (cursor.x === 0 && cursor.y === 0) {
+            this.value = this.value.slice(0, -1);
+          } else {
+            const realLines = this._clines.real.slice();
+            const fakeLines = this._clines.fake.slice();
+            const mapper = this._clines.rtof;
+
+            const currentLine = realLines.length - 1 + cursor.y;
+
+            const fakeLineIndex = mapper[currentLine];
+
+            let fakeCursorPosition = 0;
+            for (let i = 0; i <= currentLine; i++) {
+              if (mapper[i] === fakeLineIndex) {
+                fakeCursorPosition += Number(this.strWidth(realLines[i]));
+              }
+            }
+            fakeCursorPosition += cursor.x;
+
+            const realCursorPosition =
+              Number(this.strWidth(realLines[currentLine])) + cursor.x;
+
+            if (fakeLines[fakeLineIndex] === "") {
+              fakeLines.splice(fakeLineIndex, 1);
+            } else if (
+              cursor.x === -Number(this.strWidth(realLines[currentLine]))
+            ) {
+              if (currentLine > 0) {
+                const lineLengthBefore = Number(
+                  this.strWidth(realLines[currentLine - 1] ?? ""),
+                );
+
+                if (mapper[currentLine] !== mapper[currentLine - 1]) {
+                  const currentLineString = fakeLines.splice(fakeLineIndex, 1);
+                  fakeLines[fakeLineIndex - 1] += currentLineString;
+                }
+
+                const predict = this._wrapContent(
+                  fakeLines.join("\n"),
+                  Number(this.width) - Number(this.iwidth),
+                );
+
+                cursor.x = -(
+                  Number(this.strWidth(predict[currentLine - 1] ?? "")) -
+                  lineLengthBefore
+                );
+                if (predict.real.length === realLines.length) {
+                  cursor.y--;
+                }
+              }
+            } else {
+              fakeLines[fakeLineIndex] =
+                fakeLines[fakeLineIndex].slice(0, fakeCursorPosition - 1) +
+                fakeLines[fakeLineIndex].slice(fakeCursorPosition);
+              const predict = this._wrapContent(
+                fakeLines.join("\n"),
+                Number(this.width) - Number(this.iwidth),
+              );
+              cursor.x = -(
+                Number(this.strWidth(predict.real[currentLine])) -
+                realCursorPosition +
+                1
+              );
+              if (predict.real.length !== realLines.length) {
+                cursor.y++;
+              }
+            }
+            this.value = fakeLines.join("\n");
+            this.setCursor(cursor.x, cursor.y);
+          }
+        }
+      }
+    } else if (key.name === "delete") {
+      if (this.value.length) {
+        if (this.screen.fullUnicode) {
+          //
+        } else {
+          // const currentLine = this._clines.length - 1 + cursor.y;
+          if (cursor.x === 0 && cursor.y === 0) {
+            //
+          } else {
+            const realLines = this._clines.real.slice();
+            const fakeLines = this._clines.fake.slice();
+            const mapper = this._clines.rtof;
+
+            const currentLine = realLines.length - 1 + cursor.y;
+
+            const fakeLineIndex = mapper[currentLine];
+
+            let fakeCursorPosition = 0;
+            for (let i = 0; i <= currentLine; i++) {
+              if (mapper[i] === fakeLineIndex) {
+                fakeCursorPosition += Number(this.strWidth(realLines[i]));
+              }
+            }
+            fakeCursorPosition += cursor.x;
+
+            const realCursorPosition =
+              Number(this.strWidth(realLines[currentLine])) + cursor.x;
+
+            if (fakeLines[fakeLineIndex] === "") {
+              const nextLineLength = Number(
+                this.strWidth(fakeLines[fakeLineIndex + 1] ?? ""),
+              );
+              fakeLines.splice(fakeLineIndex, 1);
+              cursor.y++;
+              cursor.x = -nextLineLength;
+            } else {
+              if (fakeLineIndex < fakeLines.length - 1) {
+                if (
+                  cursor.x === -Number(this.strWidth(realLines[currentLine]))
+                ) {
+                  fakeLines[fakeLineIndex] =
+                    fakeLines[fakeLineIndex].substring(1);
+                } else {
+                  fakeLines[fakeLineIndex] =
+                    fakeLines[fakeLineIndex].slice(0, fakeCursorPosition) +
+                    fakeLines[fakeLineIndex].slice(fakeCursorPosition + 1);
+                }
+                const predict = this._wrapContent(
+                  fakeLines.join("\n"),
+                  Number(this.width) - Number(this.iwidth),
+                );
+                cursor.x = -(
+                  Number(this.strWidth(predict.real[currentLine])) -
+                  realCursorPosition
+                );
+                if (predict.real.length !== realLines.length) {
+                  cursor.y++;
+                }
+              }
+            }
+            this.value = fakeLines.join("\n");
+            this.setCursor(cursor.x, cursor.y);
+          }
         }
       }
     } else if (ch) {
       // eslint-disable-next-line no-control-regex
       if (!/^[\x00-\x08\x0b-\x0c\x0e-\x1f\x7f]$/.test(ch)) {
-        this.value += ch;
+        if (cursor.x === 0 && cursor.y === 0) {
+          this.value += ch;
+        } else if (cursor.x >= this.value.length * -1) {
+          const realLines = this._clines.real.slice();
+          const fakeLines = this._clines.fake.slice();
+          const mapper = this._clines.rtof;
+
+          const currentLine = realLines.length - 1 + cursor.y;
+
+          const fakeLineIndex = mapper[currentLine];
+          let fakeCursorPosition = 0;
+          for (let i = 0; i <= currentLine; i++) {
+            if (mapper[i] === fakeLineIndex) {
+              fakeCursorPosition += Number(this.strWidth(realLines[i]));
+            }
+          }
+          fakeCursorPosition += cursor.x;
+
+          fakeLines[fakeLineIndex] =
+            fakeLines[fakeLineIndex].slice(0, fakeCursorPosition) +
+            ch +
+            fakeLines[fakeLineIndex].slice(fakeCursorPosition);
+
+          const predict = this._wrapContent(
+            fakeLines.join("\n"),
+            Number(this.width) - Number(this.iwidth),
+          );
+          if (ch === "\n") {
+            if (predict.real.length === realLines.length) {
+              cursor.y++;
+            }
+            cursor.x = -Number(
+              this.strWidth(predict[predict.length - 1 + cursor.y]),
+            );
+          }
+
+          this.value = fakeLines.join("\n");
+          this.setCursor(cursor.x, cursor.y);
+        }
       }
     }
 
@@ -374,6 +582,43 @@ export class Textarea extends InputElement {
 
       return this.readInput(callback);
     });
+  }
+
+  getCursor() {
+    return { x: this.offsetX, y: this.offsetY };
+  }
+
+  setCursor(x: number, y: number) {
+    this.offsetX = x;
+    this.offsetY = y;
+  }
+
+  moveCursor(x: number, y: number) {
+    const prevLine = this._clines.length - 1 + this.offsetY;
+    let sync = false;
+
+    if (y <= 0 && y > this._clines.length * -1) {
+      sync = this.offsetY !== y;
+      this.offsetY = y;
+    }
+    const currentLine = this._clines.length - 1 + this.offsetY;
+    const currentText = this._clines[currentLine];
+
+    if (sync) {
+      const prevText = this._clines[prevLine];
+      const positionFromBegin = Math.max(
+        Number(this.strWidth(prevText)) + this.offsetX,
+        0,
+      );
+      x =
+        Math.max(0, Number(this.strWidth(currentText)) - positionFromBegin) *
+        -1;
+    }
+    if (x <= 0 && x >= Number(this.strWidth(currentText)) * -1) {
+      this.offsetX = x;
+    }
+    this._updateCursor(true);
+    this.screen.render();
   }
 }
 
--- types/neo-blessed/index.d.ts
+++ types/neo-blessed/index.d.ts
@@ -1,11 +1,3 @@
 declare module "neo-blessed" {
   export * from "blessed";
 }
-
-declare module "neo-blessed/lib/unicode.js" {
-  const unicode: {
-    isSurrogate(str: string, i: number): boolean;
-  };
-
-  export default unicode;
-}
